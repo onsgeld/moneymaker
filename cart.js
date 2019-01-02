3ab3ccf87f0dd8348e20dd6e1b24878fe28575dc
Tails <luuk@waalmalefijt.nl>
2019-01-02 11:18:54 +0100

    Create cart.js
    Include cart logic for Shopify

---
+++ cart.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function() {
+    $('#select-parcelshop').selectize({
+      valueField: 'parcelShopId',
+      //labelField: 'parcelShopId',
+      searchField: 'parcelShopId',
+      options: [],
+      //create: false,
+      loadThrottle: 1000,
+      maxItems: 1,
+      render: {
+        option: function(item, escape) {
+          return '<div>' +
+            '<span class="street">' + escape(item.street) + '</span> ' +
+            '<span class="houseno">' + escape(item.houseNo) + '</span>, ' +
+            '<span class="zip">' + escape(item.zipCode) + '</span> ' +
+            '<span class="city">' + escape(item.city) + '</span>' +
+          '</div>';
+        }
+      },
+      // TODO: make lowest distance best score
+      score: function(search) {
+      	var score = this.getScoreFunction(search);
+        return function(item) {
+            //return score(item) * (1 + Math.min(item.distance  / 100, 1));
+          return parseInt(item.distance);
+        };
+      },
+      // TODO: address naar veld in object parsen zodat het gebruikt kan worden als label
+      load: function(addressquery, callback) {
+        if (!addressquery.length) return callback();
+        $.ajax({
+          url: 'https://dpd.moneymaker.games/',
+          type: 'GET',
+          dataType: 'json',
+          crossDomain : true,
+          data: {
+            address: addressquery,
+          },
+          error: function() {
+            callback();
+          },
+          success: function(res) {
+            callback(Object.values(res.shops));
+          }
+        });
+      }
+    });
+  }, false);
